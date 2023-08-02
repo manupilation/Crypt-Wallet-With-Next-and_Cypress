5c55033b80194664b094dd79245b421152f94475
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-02 15:15:34 -0300

    feat: validations

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { emailChecker, passwordChecker } from '../Verificadores';
+import { emailChecker, passwordChecker } from '../validations';
 import { actionUserData } from '../actions';
 
 // Pedi ajuda ao Gessé, muito obrigado !
