9283cec0ad2dc020cae3b51f38db5ba51c603791
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-02 20:03:04 -0300

    refactor: Table class to func

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,54 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import TableBody from './TableBody';
 
-class Table extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Table = () => {
+  const headers = [
+    'Descrição',
+    'Tag',
+    'Método de pagamento',
+    'Valor',
+    'Moeda',
+    'Câmbio utilizado',
+    'Valor convertido',
+    'Moeda de conversão',
+    'Editar/Excluir',
+  ];
 
-  renderTable() {
-    const headers = [
-      'Descrição',
-      'Tag',
-      'Método de pagamento',
-      'Valor',
-      'Moeda',
-      'Câmbio utilizado',
-      'Valor convertido',
-      'Moeda de conversão',
-      'Editar/Excluir',
-    ];
-
-    return (
+  return (
+    <section>
       <table>
         <thead>
           <tr>
-            { headers.map((header, i) => <th key={ i }>{header}</th>) }
+            {headers.map((header, i) => (
+              <th key={i}>{header}</th>
+            ))}
           </tr>
         </thead>
         <TableBody />
       </table>
-    );
-  }
+    </section>
+  );
+};
 
-  render() {
-    return (
-      <section>
-        { this.renderTable() }
-      </section>
-    );
-  }
-}
-
-Table.propTypes = {
-  getExpenses: PropTypes.arrayOf(PropTypes.object),
-}.isRequired;
-
-const mapStateToProps = (state) => ({
-  getExpenses: state.wallet.expenses,
-});
-
-export default connect(mapStateToProps)(Table);
+export default Table;
