0feb388ab040236059171c26d4e0a1ff5509c169
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-02 20:02:57 -0300

    refactor: Select class to func

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Select extends React.Component {
-  render() {
-    const { name, labelText, id, options, onChange } = this.props;
-    return (
-      <label htmlFor={ id }>
-        { labelText }
-        <select
-          name={ name }
-          id={ id }
-          onChange={ onChange }
-        >
-          {
-            options.map((op, i) => (
-              <option
-                key={ i }
-                value={ op }
-              >
-                {op}
-              </option>
-            ))
-          }
-        </select>
-      </label>
-    );
-  }
-}
+const Select = ({ name, labelText, id, options, onChange }) => {
+  return (
+    <label htmlFor={id}>
+      {labelText}
+      <select
+        name={name}
+        id={id}
+        onChange={onChange}
+      >
+        {
+          options.map((op, i) => (
+            <option
+              key={i}
+              value={op}
+            >
+              {op}
+            </option>
+          ))
+        }
+      </select>
+    </label>
+  );
+};
 
 Select.propTypes = {
   name: PropTypes.string,
