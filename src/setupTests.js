9085305086b917427725d79c216311e57515bbcc
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-02 13:53:36 -0300

    feat: setup tests jest

---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom/extend-expect';
+import MutationObserver from '@sheerun/mutationobserver-shim';
+
+window.MutationObserver = MutationObserver;
+require('jest-localstorage-mock');
