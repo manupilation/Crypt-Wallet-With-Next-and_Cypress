2609f58104e125676556465cd75843ef56c2922d
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-02 13:52:46 -0300

    feat: App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import { Login, Wallet } from './pages';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
+    </Switch>
+  );
+}
+
+export default App;
