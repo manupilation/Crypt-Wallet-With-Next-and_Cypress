8b503d51d6d9124cc93642d9b0fa22d8973d6494
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-04 14:54:15 -0300

    feat: now loginPage is using Meta component

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Provider } from "react-redux";
 import store from "../../Store";
 import Login from "../../components/Login";
+import Meta from '../../infra/Meta';
+import Head from 'next/head';
 
 function LoginPage() {
   return (
     <Provider store={store}>
+      <Meta />
       <Login />
     </Provider>
   );
