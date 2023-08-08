615d5a9ed7f1305ab926d3855bae82b58474a0d1
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-08 17:36:48 -0300

    fix: import localStorageMock with module

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
 import MutationObserver from '@sheerun/mutationobserver-shim';
+import 'jest-localstorage-mock';
 
 window.MutationObserver = MutationObserver;
-require('jest-localstorage-mock');
