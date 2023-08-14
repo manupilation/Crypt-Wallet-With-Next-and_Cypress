1d861f812e7b92680d71d39bfd296b797ad6e0cf
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-14 17:06:55 -0300

    tests: TableBody

--- src/tests/Components/TableBody.test.jsx
+++ src/tests/Components/TableBody.test.jsx
@@ -1,18 +1,65 @@
 import React from 'react';
-import { initialStateHeader } from '../__mocks__/mockData';
+import { initialStateHeader, initialStateWithExpenses } from '../__mocks__/mockData';
 import {expect} from '@jest/globals';
 import { renderWithStore } from '../testConfig';
 import TableBody from '../../components/TableBody';
+import TableHeaderMock from '../__mocks__/tableHeader';
+import * as ReactRedux from 'react-redux';
+import { fireEvent, screen } from '@testing-library/react';
+
+const mockDispatch = jest.fn();
+let dispatchSpyon;
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
 
 describe('Tests TableBody component', () => {
-  beforeEach(() => jest.clearAllMocks());
-  test('Tests if tableBody is rendered without errors', () => {
-    const tableBody = renderWithStore(
-    <table>
-      <TableBody />
-    </table>,
-    initialStateHeader);
+  let tableBody;
 
+  beforeEach(() => {
+    dispatchSpyon = jest.spyOn(ReactRedux, 'useDispatch').mockImplementation(() => mockDispatch);
+
+    tableBody = renderWithStore(
+      <table>
+        <TableHeaderMock />
+        <TableBody />
+      </table>,
+      initialStateWithExpenses);
+  });
+
+  afterEach(() => {
+    tableBody.unmount();
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  })
+
+  beforeAll(() => jest.clearAllMocks());
+
+  test('Tests if tableBody is rendered without errors', () => {
     expect(tableBody.container).toBeInTheDocument();
+  });
+
+  test('Tests if delete button activate handleClick function sending dispatch', () => {
+    const deleteBtn = screen.getAllByRole('button', {
+      name: /excluir/i
+    })[0];
+
+    fireEvent.click(deleteBtn);
+
+    expect(dispatchSpyon).toBeCalled();
+  });
+
+  test('Tests if editar button activate handleUpdate function sending dispatch', () => {
+    const updateBtn = screen.getAllByRole('button', {
+      name: /editar/i
+    })[0];
+
+    fireEvent.click(updateBtn);
+
+    expect(dispatchSpyon).toBeCalled();
   });
 });
