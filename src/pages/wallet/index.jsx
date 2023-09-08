d8301d6e97989066e2a7719eed57f6fc50e1467a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-08 14:33:07 -0300

    refactor: wallet page using Meta Tags

--- src/pages/wallet/index.jsx
+++ src/pages/wallet/index.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../../Store';
 import Wallet from '../../components/Wallet';
+import Meta from '../../infra/Meta';
 
 function WalletPage() {
     return (
       <Provider store={store}>
+        <Meta 
+          title="Crypt Wallet - Your Wallet" 
+          description="Manage your personal Wallet"
+        />
         <Wallet />
       </Provider>
     );
